b314b298ae6cc3a2f8aabd62eca354d985504b59
shreyaghate02 <shreya.ghate.sg@gmail.com>
2020-05-31 13:15:05 +0530

    Add: Files for tests and README.md

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    }
+  }
+
+  makeIncrementer = amount => () =>
+    this.setState(prevState => ({
+      count: prevState.count + amount,
+    }));
+
+  increment = this.makeIncrementer(1);
+
+  decrement = this.makeIncrementer(-1);
+
+  render() {
+    return (
+      <div>
+        <h1>Count: {this.state.count}</h1>
+        <button className="increment" onClick={this.increment}>Increment</button>
+        <button className="decrement" onClick={this.decrement}>Decrement</button>
+      </div>
+    )
+  }
 }
 
 export default App;
